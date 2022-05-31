47b1db8d272e3212d29e046418b4299be515cf72
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2022-05-31 16:26:57 +0300

    [Intermediate commit][refactoring]

--- src/s21_main_cat.c
+++ src/s21_main_cat.c
@@ -3,24 +3,18 @@
 int main(int argc, char *argv[]) {
 	struct activeFlags CurrentFlags;
     struct activeFlags *pCurrentFlags = &CurrentFlags;
-	// Ищем какие флаги есть
+
 	searchArgs(argc, argv, pCurrentFlags);
 
 	printf("\n");
 
-	//Ищем отступ аргументов для файлов
 	int offset = offsetOnRightSiteOfArguments(argc, argv);
 
 	printf("\n");
 
-	int isValid = 1;
-
-	if (isValidFiles(offset, argc, argv) != 1) {
-		printf("У вас файл хуйня! >:(\n");
-		isValid = 0;
-	}
-
-	if (isValid) {
+	if (isValidFiles(offset, argc, argv) == 1) {
 		flagHandling(argc, argv, pCurrentFlags);
 	}
+
+	return 0;
 }
