1ea96b7be4c0b7eb8e0457627f121a02e94cee32
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2022-06-03 05:56:03 +0300

    [bugfix]

--- src/s21_cat.h
+++ src/s21_cat.h
@@ -17,10 +17,6 @@ struct activeFlags {
 void init(struct activeFlags *pCurrentFlags);
 // Ищет аргументы
 void searchArgs(int argc, char *argv[], struct activeFlags *pCurrentFlags);
-// Ищет отступ для понимания, откуда начинаются идти аргументы файлов
-int offsetOnRightSiteOfArguments(int argc, char *argv[]);
-// Если файлы в аргументах есть, проверяет их
-int isValidFiles(int offset, int argc, char *argv[]);
 // Проверка на случай, если передано 0 флагов
 int checkNullFlags(struct activeFlags *pCurrentFlags);
 // Функция запускающая другие функции, зависимо от флагов
@@ -28,6 +24,8 @@ void flagHandling(int argc, char *argv[], struct activeFlags *pCurrentFlags);
 // Создание и удаление копий файлов из аргументов
 void createCopyFiles(int argc, char *argv[]);
 void deleteCopyFiles();
+// Создание несуществующего файла
+void createEmptyFile();
 // Обработка случая без флагов
 void withoutFlags(int argc, char *argv[]);
 // Обработка флага -s
