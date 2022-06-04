a2c2cb53873c18ba37bbb09633e4ab07f1a1299c
Nikolai <dolaxom4@gmail.com>
2022-06-04 20:06:59 +0300

    [new project stuct]

--- src/s21_cat.c
+++ src/s21_cat.c
@@ -43,416 +43,67 @@ void searchArgs(int argc, char *argv[], struct activeFlags *pCurrentFlags) {
     }
 }
 
-int checkNullFlags(struct activeFlags *pCurrentFlags) {
-    int tmp = 0;
-    if (pCurrentFlags->b == 0 && pCurrentFlags->e == 0
-    && pCurrentFlags->n == 0 && pCurrentFlags->s == 0
-    && pCurrentFlags->t == 0) {
-        tmp = 1;
-    }
-
-    return tmp;
-}
-
 void flagHandling(int argc, char *argv[], struct activeFlags *pCurrentFlags) {
-    createCopyFiles(argc, argv);
-
-    if (pCurrentFlags->s == 1) {
-        flagS_Activate();
-    }
-
-    if (pCurrentFlags->b == 1) {
-        flagB_Activate();
-    }
-
-    if (pCurrentFlags->n == 1) {
-        flagN_Activate();
-    }
-
-    if (pCurrentFlags->t == 1) {
-        flagT_Activate();
-    }
-
-    if (pCurrentFlags->e == 1) {
-        flagE_Activate();
-    }
-    output();
-    //deleteCopyFiles();
-}
-
-void createCopyFiles(int argc, char *argv[]) {
-    const int MAX_LINE = 999999;
-    char temp_filename[5000];
-    FILE *file, *temp;
-    char buffer[MAX_LINE];
-    int stepsPrintA = 1;
-    int canContinue = 0;
-    for (int count = 1; count < argc; count++) {
-        int isNullFile = 0;
-        if ((file = fopen(argv[count], "r")) == NULL) {
-            isNullFile = 1;
-            if (canContinue == 0) {
-                continue;
-            }
-        }
-        
-        canContinue = 1;
-        char nameOfFile[200];
-        strcpy(nameOfFile, argv[count]);
-        char tmp[2000] = "";
-
-        for (int z = 0; z < stepsPrintA; z++) {
-            tmp[z] = 'A';
-        }
-
-        stepsPrintA++;
-        strcpy(nameOfFile, tmp);
-        strcpy(temp_filename, "./tmp/tmp_");
-        strcat(temp_filename, nameOfFile);
-        if (isNullFile == 1) {
-            strcat(temp_filename, "");
-        }
-
-        mkdir("tmp", S_IRWXU);
-
-        temp = fopen(temp_filename, "w");
-
-        if (temp == NULL) {
-            printf("error %s\n", temp_filename);
-        }
-        if (isNullFile == 0) {
-            while (fgets(buffer, MAX_LINE, file) != NULL) {
-                fprintf(temp, "%s", buffer);
-            }
-        } else {
-            fprintf(temp, "\ncat: %s: No such file or directory\n", argv[count]);
-        }
-
-        fclose(file);
-        fclose(temp);
-    }
-}
-
-void deleteCopyFiles() {
-    char temp_filename[5000];
-    DIR *folder;
-    struct dirent *entry;
-
-    folder = opendir("tmp");
-    if(folder == NULL) {
-        perror("Нет такой папки\n");
-    }
-
-    while ((entry=readdir(folder))) {
-        strcpy(temp_filename, "./tmp/");
-        strcat(temp_filename, entry->d_name);
-        remove(temp_filename);
-    }
-
-    closedir(folder);
-
-    rmdir("tmp");
-}
-
-void flagS_Activate() {
-    FILE *file, *temp;
-    const int MAX_LINE = 999999;
-    char buffer[MAX_LINE];
-    char filename[2000];
-    char temp_filename[2000];
-
-    DIR *folder;
-    struct dirent *entry;
-    folder = opendir("tmp");
-
-    while ((entry=readdir(folder))) {
-        if (entry->d_name[0] == '.') {
-            continue;
-        }
-
-        size_t length = strlen(entry->d_name);
-        if (entry->d_name[length] == '_') {
-            continue;
-        }
-
-        strcpy(filename, "./tmp/");
-        strcat(filename, entry->d_name);
-        strcpy(temp_filename, "./tmp/tmp____");
-        strcat(temp_filename, entry->d_name);
-
-        file = fopen(filename, "r");
-        temp = fopen(temp_filename, "w");
-
-        if (file == NULL || temp == NULL) {
-            fclose(file);
-            fclose(temp);
-            continue;
-        }
-
-        int flagToNullStr = 0;
-
-        while (fgets(buffer, MAX_LINE, file)) {
-            if (strlen(buffer) > 1 || buffer[0] == '\t') {
-                flagToNullStr = 0;
-                fprintf(temp, "%s", buffer);
-            } else {
-                if (flagToNullStr != 1) {
-                    fprintf(temp, "%s", buffer);
-                }
-                flagToNullStr = 1;
-            }
-        }
-
-        fclose(temp);
-        fclose(file);
-
-        remove(filename);
-        rename(temp_filename, filename);
-    }
-}
-
-void flagN_Activate() {
-    FILE *file, *temp;
-    const int MAX_LINE = 999999;
-    char buffer[MAX_LINE];
-    char filename[2000];
-    char temp_filename[2000];
-
-    DIR *folder;
-    struct dirent *entry;
-    folder = opendir("tmp");
-
-    while ((entry=readdir(folder))) {
-        if (entry->d_name[0] == '.') {
-            continue;
-        }
-
-        size_t length = strlen(entry->d_name);
-        if (entry->d_name[length - 1] == '_') {
-            continue;
-        }
-
-        strcpy(filename, "./tmp/");
-        strcat(filename, entry->d_name);
-        strcpy(temp_filename, "./tmp/tmp____");
-        strcat(temp_filename, entry->d_name);
-
-        file = fopen(filename, "r");
-        temp = fopen(temp_filename, "w");
-
-        if (file == NULL || temp == NULL) {
-            fclose(file);
-            fclose(temp);
-            continue;
-        }
-
-        int current_line = 1;
-        while (fgets(buffer, MAX_LINE, file)) {
-            fprintf(temp, "%6d\t%s", current_line++, buffer);
-        }
-
-        fclose(temp);
-        fclose(file);
-
-        remove(filename);
-        rename(temp_filename, filename);
-    }
-}
-
-void flagB_Activate() {
-    FILE *file, *temp;
-    const int MAX_LINE = 999999;
-    char buffer[MAX_LINE];
-    char filename[2000];
-    char temp_filename[2000];
-
-    DIR *folder;
-    struct dirent *entry;
-    folder = opendir("tmp");
-
-    while ((entry=readdir(folder))) {
-        if (entry->d_name[0] == '.') {
-            continue;
-        }
-
-        size_t length = strlen(entry->d_name);
-        if (entry->d_name[length] == '_') {
-            continue;
-        }
-
-        strcpy(filename, "./tmp/");
-        strcat(filename, entry->d_name);
-        strcpy(temp_filename, "./tmp/tmp____");
-        strcat(temp_filename, entry->d_name);
-
-        file = fopen(filename, "r");
-        temp = fopen(temp_filename, "w");
-
-        if (file == NULL || temp == NULL) {
-            fclose(file);
-            fclose(temp);
-            continue;
-        }
-
-        int current_line = 1;
-        while (fgets(buffer, MAX_LINE, file)) {
-            if (strlen(buffer) > 1 || buffer[0] == '\t') {
-                fprintf(temp, "%6d\t%s", current_line++, buffer);
-            } else {
-                fprintf(temp, "%s", buffer);
-            }
-        }
-
-        fclose(temp);
-        fclose(file);
-
-        remove(filename);
-        rename(temp_filename, filename);
-    }
-}
-
-void flagT_Activate() {
-    FILE *file, *temp;
-    const int MAX_LINE = 2000;
-    char filename[MAX_LINE];
-    char temp_filename[MAX_LINE];
-    int chr;
-
-    DIR *folder;
-    struct dirent *entry;
-    folder = opendir("tmp");
-
-    while ((entry=readdir(folder))) {
-        if (entry->d_name[0] == '.') {
-            continue;
-        }
-
-        size_t length = strlen(entry->d_name);
-        if (entry->d_name[length] == '_') {
-            continue;
-        }
-
-        strcpy(filename, "./tmp/");
-        strcat(filename, entry->d_name);
-        strcpy(temp_filename, "./tmp/tmp____");
-        strcat(temp_filename, entry->d_name);
-
-        file = fopen(filename, "r");
-        temp = fopen(temp_filename, "w");
-
-        if (file == NULL || temp == NULL) {
-            fclose(file);
-            fclose(temp);
-            continue;
-        }
-
-        int i = 0;
-        while((chr = getc(file)) != EOF) {
-            if ((int)chr == 9 && i > 6) {
-                fprintf(temp, "%s", "^I");
-            } else {
-                fprintf(temp, "%c", chr);
-            }
-            i++;
-
-            if ((int)chr == 10) {
-                i = 0;
-            }
-        }
-
-        fclose(temp);
-        fclose(file);
-
-        remove(filename);
-        rename(temp_filename, filename);
-    } 
-}
-
-void flagE_Activate() {
-    FILE *file, *temp;
-    const int MAX_LINE = 2000;
-    char filename[MAX_LINE];
-    char temp_filename[MAX_LINE];
-    int chr;
-
-    DIR *folder;
-    struct dirent *entry;
-    folder = opendir("tmp");
-
-    while ((entry=readdir(folder))) {
-        if (entry->d_name[0] == '.') {
-            continue;
-        }
-
-        size_t length = strlen(entry->d_name);
-        if (entry->d_name[length] == '_') {
-            continue;
-        }
-
-        strcpy(filename, "./tmp/");
-        strcat(filename, entry->d_name);
-        strcpy(temp_filename, "./tmp/tmp____");
-        strcat(temp_filename, entry->d_name);
-
-        file = fopen(filename, "r");
-        temp = fopen(temp_filename, "w");
-
-        if (file == NULL || temp == NULL) {
-            fclose(file);
-            fclose(temp);
-            continue;
-        }
-
-        while((chr = getc(file)) != EOF) {
-            if (chr == '\n') {
-                fprintf(temp, "%c", 36);
-                fprintf(temp, "%c", chr);
-            } else {
-                fprintf(temp, "%c", chr);
-            }
-        }
-
-        fclose(temp);
-        fclose(file);
-
-        remove(filename);
-        rename(temp_filename, filename);
-    }
-}
-
-void output() {
-    struct dirent **namelist;
-    int n;
     FILE *file;
-    char filename[2000];
-    int chr;
+    const int MAX_LINE = 999999;
+    char original[MAX_LINE];
+    char buffer[MAX_LINE];
 
-    n = scandir("tmp", &namelist, 0, alphasort);
-    if (n < 0) {
-        perror("scandir");
-    } else {
-        while (n--) {
-            if (namelist[n]->d_name[0] == '.') {
-                continue;
+    for (int count = 1; count < argc; count++) {
+        if((file = fopen(argv[count], "r")) == NULL) {
+            continue;
+        }
+        int current_line = 1;
+        int flagToNullStr = 0;
+        while (fgets(original, MAX_LINE, file)) {
+            if (pCurrentFlags->s == 1) {
+                if (strlen(original) > 1 || original[0] == '\t') {
+                    flagToNullStr = 0;
+                } else {
+                    if (flagToNullStr == 1) {
+                        continue;
+                    }
+                    flagToNullStr = 1;
+                }
             }
-            strcpy(filename, "./tmp/");
-            strcat(filename, namelist[n]->d_name);
-
-            file = fopen(filename, "r");
-            if (file == NULL) {
-                fclose(file);
-                continue;
+            if (pCurrentFlags->b == 1) {
+                strcpy(buffer, original);
+                if (strlen(buffer) > 1 || buffer[0] == '\t') {
+                    sprintf(buffer, "%6d\t%s", current_line++, original);
+                } else {
+                    if (pCurrentFlags->e == 1) {
+                        sprintf(buffer, "      \t%s", original);
+                    } else {
+                        sprintf(buffer, "%s", original);
+                    }
+                }
+                strcpy(original, buffer);
             }
-            //printf("\nITS TIME FOR %s\n", filename);
-            while((chr = getc(file)) != EOF) {
-                fprintf(stdout, "%c", chr);
+            if (pCurrentFlags->n == 1) {
+                sprintf(buffer, "%6d\t%s", current_line++, original);
+                strcpy(original, buffer);
             }
-
-            fclose(file);
-
-            free(namelist[n]);
+            if (pCurrentFlags->t == 1) {
+                strcpy(buffer, original);
+                char *ptr = strchr(buffer, '\t');
+                if (ptr != NULL) {
+                    *ptr = '^';
+                    *(ptr + 1) = 'I';
+                }
+                strcpy(original, buffer);     
+            }
+            if (pCurrentFlags->e == 1) {
+                strcpy(buffer, original);
+                char *ptr = strchr(buffer, '\n');
+                if (ptr != NULL) {
+                    *ptr = '$';
+                    strcat(buffer, "\n");
+                }
+                strcpy(original, buffer);
+            }
+            printf("%s", original);
         }
 
-        free(namelist);
+        fclose(file);
     }
 }
